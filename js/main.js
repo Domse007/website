f7d07c014fe9d74482627b3abb5bf13794827f68
Domse007 <dominikkeller55@gmail.com>
2023-08-19 22:55:55 +0200

    Fixed search bar and layout.

--- js/main.js
+++ js/main.js
@@ -10,8 +10,6 @@ report()
 const searchInput = document.getElementById("search");
 const list = document.getElementById("list");
 
-let LAST_INPUT = null;
-
 searchInput.addEventListener("input", (e) => {
     const input = e.target.value.trim().toLowerCase();
 
@@ -24,45 +22,22 @@ searchInput.addEventListener("input", (e) => {
             resultItem.classList.add('result-item');
             // grabbing the name of the current point of the loop and adding the
 	    // name as the list item's text
-            const text = document.createTextNode(key);
-            // appending the text to the result item
-            resultItem.appendChild(text);
-	    
-	    // // adding link to item
-	    // resultItem.setAttribute("href", "http://www.digitec.ch/");
-	    resultItem.href = "http://www.github.com/";
-	    
+            const anchor = document.createElement('a');
+
+            anchor.href = `./${value}`;
+            anchor.innerText = key;
+            
+            resultItem.appendChild(anchor);
+            
             // appending the result item to the list
             list.appendChild(resultItem);
 	}
     }
-    
-    
-    // const posts = gen.POSTS.filter((post) => post.toLowerCase().includes(value));
-
-    // setList(posts);
 });
 
-const setList = (posts) => {
-    for (const post of posts) {
-        // creating a li element for each result item
-        const resultItem = document.createElement('div');
-
-        // adding a class to each item of the results
-        resultItem.classList.add('result-item');
-
-        // grabbing the name of the current point of the loop and adding the name as the list item's text
-        const text = document.createTextNode(post);
-
-        // appending the text to the result item
-        resultItem.appendChild(text);
-
-        // appending the result item to the list
-        list.appendChild(resultItem);
+searchInput.addEventListener("blur", (e) => {
+    const input = e.target.value.trim().toLowerCase();
+    if (input.length() == 0) {
+        list.replaceChildren();
     }
-}
-
-searchInput.addEventListener("blur", (_e) => list.replaceChildren());
-
-// Maybe improvable?
-document.getElementById("search-div-wrapper").style.height = document.getElementById("search").style.height + 20;
+});
